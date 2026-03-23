3a6d9b939f337dc259120c15d0fa75e7f20cc608
Abrar Butt <abrarbuttmi7@gmail.com>
2026-03-23 23:21:13 +0500

    modified js

--- javascript/index.js
+++ javascript/index.js
@@ -10,4 +10,4 @@ const library = {
 console.log(library.name); // Output: rich dad poor dad
 console.log(library.version); // Output: 1.0.0
 console.log(library.description); // Output: A simple JavaScript library
-console.log(library.discount(100, 85)); // Output: 85
+console.log(library.discount(100, 899)); // Output: 85
