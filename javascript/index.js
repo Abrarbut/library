8b46ce21c479216001e6d7f068d8b41ba989698f
Abrar Butt <abrarbuttmi7@gmail.com>
2026-03-25 02:15:06 +0500

    fix: correct element IDs for author and pages input fields in index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,24 +1,45 @@
-const titleInput = document.getElementById("title");
-const authorInput = document.getElementById("author");
-const pagesInput = document.getElementById("pages");
-const addButton = document.getElementById("addButton");
+// Get HTML elements
+const titleInput = document.getElementById("book-title");
+const authorInput = document.getElementById("book-author");
+const pagesInput = document.getElementById("book-pages");
+const doneButton = document.getElementById("doneButton");
 const bookListDiv = document.getElementById("bookList");
 
+// When user clicks "Done" button
+doneButton.addEventListener("click", function() {
+  // Get values from input fields
+  const title = titleInput.value;
+  const author = authorInput.value;
+  const pages = pagesInput.value;
+  
+  // Check if all fields have values
+  if (title && author && pages) {
+    // Add book to library
+    addBookToLibrary(title, author, pages);
+    
+    // Clear input fields
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    
+    // Display the books
+    displayBooks();
+  } else {
+    alert("Please fill in all fields!");
+  }
+});
 
-
-
-
-
-const myLibrary = [];
-
-function Book(title, author, pages, id) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.id = id || crypto.randomUUID(); 
-}
-
-function addBookToLibrary(title, author, pages) {
-  const newBook = new Book(title, author, pages);
-  myLibrary.push(newBook);
+// Function to display all books
+function displayBooks() {
+  bookListDiv.innerHTML = "";
+  
+  myLibrary.forEach((book) => {
+    const bookDiv = document.createElement("div");
+    bookDiv.innerHTML = `
+      <h3>${book.title}</h3>
+      <p>Author: ${book.author}</p>
+      <p>Pages: ${book.pages}</p>
+    `;
+    bookListDiv.appendChild(bookDiv);
+  });
 }
