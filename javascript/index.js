1f222a89513f8f6f205b247ddd71eda15e36cfe6
Abrar Butt <abrarbuttmi7@gmail.com>
2026-03-25 02:05:19 +0500

    refactor: remove unused library object and clean up index.js for book management

--- javascript/index.js
+++ javascript/index.js
@@ -1,19 +1,24 @@
-const library = {
-  name: "rich dad poor dad",
-  version: "1.0.0",
-  description: "A simple JavaScript library",
-  final : 100,
-  price: 50,
-  percentage: 50,
-  discount : function(price, percentage) {
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const addButton = document.getElementById("addButton");
+const bookListDiv = document.getElementById("bookList");
 
-    this.final =  price - (price * (percentage / 100));
-  }
+
+
+
+
+
+const myLibrary = [];
+
+function Book(title, author, pages, id) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.id = id || crypto.randomUUID(); 
 }
 
-library.discount(library.price, library.percentage);
-
-console.log(library.name); // Output: rich dad poor dad
-console.log(library.version); // Output: 1.0.0
-console.log(library.description); // Output: A simple JavaScript library
-console.log(library.final); // Output: 85
+function addBookToLibrary(title, author, pages) {
+  const newBook = new Book(title, author, pages);
+  myLibrary.push(newBook);
+}
