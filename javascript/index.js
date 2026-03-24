be5005f84dfa4ae1de2a6dfb68805c3e2f541f2f
Abrar Butt <abrarbuttmi7@gmail.com>
2026-03-25 02:30:24 +0500

    bug fixed for id input and to show it

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,25 @@
+// Array to store books
+const myLibrary = [];
+
+// Book Constructor
+function Book(title, author, pages, id) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.id = id || crypto.randomUUID();
+}
+
+// Function to add book to library
+function addBookToLibrary(title, author, pages, id) {
+  const newBook = new Book(title, author, pages, id);
+  myLibrary.push(newBook);
+}
+
 // Get HTML elements
 const titleInput = document.getElementById("book-title");
 const authorInput = document.getElementById("book-author");
 const pagesInput = document.getElementById("book-pages");
+const idInput = document.getElementById("book-id");
 const doneButton = document.getElementById("doneButton");
 const bookListDiv = document.getElementById("bookList");
 
@@ -11,17 +29,17 @@ doneButton.addEventListener("click", function() {
   const title = titleInput.value;
   const author = authorInput.value;
   const pages = pagesInput.value;
-  
+  const id = idInput.value;
   // Check if all fields have values
-  if (title && author && pages) {
+  if (title && author && pages || id) {
     // Add book to library
-    addBookToLibrary(title, author, pages);
+    addBookToLibrary(title, author, pages, id);
     
     // Clear input fields
-    titleInput.value = "";
-    authorInput.value = "";
-    pagesInput.value = "";
-    
+    titleInput.value = title;
+    authorInput.value = author;
+    pagesInput.value = pages;
+
     // Display the books
     displayBooks();
   } else {
@@ -39,6 +57,7 @@ function displayBooks() {
       <h3>${book.title}</h3>
       <p>Author: ${book.author}</p>
       <p>Pages: ${book.pages}</p>
+      <p>id: ${book.id}</p>
     `;
     bookListDiv.appendChild(bookDiv);
   });
